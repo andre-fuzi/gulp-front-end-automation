4bf852dc15462cfb74e41df9b88930d01fff0d29
André Kaoru Fuziyama <andrefuzi@MacBook-Pro-de-Andre.local>
2020-02-19 07:39:47 -0300

    add src folder | adjusted sassCompile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
 
 function sassCompiler() {
-  return gulp.src('./css/scss/*.scss')
+  return gulp.src('./src/**/*.scss')
     .pipe(sass({outputStyle: "compressed"}))
     .pipe(autoprefixer({
       cascade: false
     }))
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest('./dist/css/'));
 };
 
 gulp.task('sass', sassCompiler);

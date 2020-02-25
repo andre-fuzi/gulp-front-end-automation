dfb743fc280cd852ceba000f8d4ef00328cb0794
André Kaoru Fuziyama <andrefuzi@MacBook-Pro-de-Andre.local>
2020-02-25 20:34:53 -0300

    Add jsCompiler func and browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
+const broswerSync = require('browser-sync').create();
+const babel = require('gulp-babel');
 
 function sassCompiler() {
   return gulp.src('./src/**/*.scss')
@@ -9,13 +11,39 @@ function sassCompiler() {
     .pipe(autoprefixer({
       cascade: false
     }))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(broswerSync.stream());
 };
 
-gulp.task('sass', sassCompiler);
+gulp.task('sass-compiler', sassCompiler);
+
+function jsCompiler() {
+  return gulp.src('./src/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(babel({
+      "presets": ["@babel/preset-env"]
+    }))
+    .pipe(gulp.dest('./dist/js/'));
+}
+
+gulp.task('js-compiler', jsCompiler);
+
+function broswerSyncInit () {
+  broswerSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+}
+
+gulp.task('browser-sync', broswerSyncInit);
 
 function watch() {
-  gulp.watch('./css/scss/*.scss', sassCompiler)
+  gulp.watch('./src/**/*.scss', sassCompiler);
+  gulp.watch('./src/**/*.js', jsCompiler);
+  gulp.watch(['*.html', '*.php']).on('change', broswerSync.reload);
 };
 
-gulp.task('default', watch);
+gulp.task('watch', watch);
+
+gulp.task('default', gulp.parallel('sass-compiler', 'js-compiler', 'watch', 'browser-sync'));
